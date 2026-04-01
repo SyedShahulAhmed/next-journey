7b2ccc5483d2bcf70285ea87a6fa4a8347e544ee
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2026-04-01 20:27:58 +0530

    add : added pomodoro quotes and images data

---
+++ 01_pomodoro-timer/data/gifs.ts
@@ -0,0 +1,9 @@
+export const gifList: string[] = [
+  "/images/two.jpg",
+  "/images/one.jpg",
+  "/images/three.jpg",
+  "/images/four.jpg",
+  "/images/five.jpg",
+  "/images/six.jpg",
+  "/images/seven.jpg",
+];
---
+++ 01_pomodoro-timer/data/quotes.ts
@@ -0,0 +1,33 @@
+export const quotes: string[] = [
+  // 🎬 Anime
+  "Power comes in response to a need, not a desire. – Dragon Ball Z",
+  "Fear is not evil. It tells you what your weakness is. – Fairy Tail",
+  "Whatever you lose, you’ll find it again. But what you throw away you’ll never get back. – Kenshin Himura",
+  "If you don’t take risks, you can’t create a future. – Monkey D. Luffy",
+  "A person grows when he’s able to overcome hardships. – Jiraiya",
+  "Knowing you’re different is only the beginning. – Tokyo Ghoul",
+  "No matter how deep the night, it always turns to day, eventually. – One Piece",
+  "The world isn’t perfect. But it’s there for us, doing the best it can. – Roy Mustang",
+
+  // 🎥 Movies
+  "Why do we fall? So we can learn to pick ourselves up. – Batman Begins",
+  "Do or do not. There is no try. – Star Wars",
+  "Great men are not born great, they grow great. – The Godfather",
+  "With great power comes great responsibility. – Spider-Man",
+  "Hope is a good thing, maybe the best of things. – The Shawshank Redemption",
+  "Every man dies, not every man really lives. – Braveheart",
+  "It’s not who I am underneath, but what I do that defines me. – Batman Begins",
+  "Get busy living, or get busy dying. – The Shawshank Redemption",
+  "Your time is limited, don’t waste it living someone else’s life. – Steve Jobs ",
+
+  // 🎮 Games
+  "Stand in the ashes of a trillion dead souls and ask if honor matters. – Mass Effect",
+  "The right man in the wrong place can make all the difference. – Half-Life 2",
+  "Endure and survive. – The Last of Us",
+  "A hero need not speak. When he is gone, the world will speak for him. – Halo",
+  "Nothing is true, everything is permitted. – Assassin’s Creed",
+  "War. War never changes. – Fallout",
+  "We all make choices, but in the end our choices make us. – BioShock",
+  "The courage to walk into the darkness, but strength to return to the light. – Final Fantasy",
+  "Even in dark times, we cannot relinquish the things that make us human. – Metro",
+];
