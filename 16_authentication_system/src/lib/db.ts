edaa3eec492e35ee9e580a0c4194adab10e4c218
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2026-05-05 20:36:40 +0530

    Add : Database config and jwt generator

---
+++ 16_authentication_system/src/lib/db.ts
@@ -0,0 +1,6 @@
+import mongoose from "mongoose";
+
+export const connectDb = async () => {
+  if (mongoose.connection.readyState >= 1) return;
+  await mongoose.connect(process.env.MONGODB_URI!);
+};
