ca3edf70d2cdac17cd9440a3903940ae1302e236
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2026-05-07 21:02:29 +0530

    Add : middleware protected routes

---
+++ 17-expense-tracker-api/src/middleware/middleware.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const token = req.cookies.get("token")?.value;
+
+  const isAuthPage = req.nextUrl.pathname.startsWith("/login");
+  req.nextUrl.pathname.startsWith("/signup");
+  const isProtected = req.nextUrl.pathname.startsWith("/dashboard");
+
+  if (isProtected && !token) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  if (isAuthPage && token) {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/dashboard/:path", "/login", "signup"],
+};
