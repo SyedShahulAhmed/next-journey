175a61ca290f9728f351abc44707839cb3327a64
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2026-05-12 19:49:15 +0530

    add : default layout

---
+++ 18-url-shortener-api/src/app/layout.tsx
@@ -0,0 +1,18 @@
+import "../styles/globals.css";
+
+export const metadata = {
+  title: "Shortly",
+  description: "Premium URL Shortener",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
