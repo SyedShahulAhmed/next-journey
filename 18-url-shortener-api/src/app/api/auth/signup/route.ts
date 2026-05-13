691e5ff647f721256ed3a154fbcc4f3b530e6a1e
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2026-05-13 22:30:40 +0530

    add : Signup route

---
+++ 18-url-shortener-api/src/app/api/auth/signup/route.ts
@@ -0,0 +1,34 @@
+import { connectDb } from "@/lib/db";
+import User from "@/models/User";
+import bcrypt from "bcryptjs";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    await connectDb();
+
+    const { name, email, password } = await req.json();    
+
+    if(!name && ! email && !password) {
+      return NextResponse.json({error : "Some field are missing"},{status : 400})
+    }                            
+    const userExist = await User.findOne({email});
+
+    if(userExist) {
+      return NextResponse.json({error : "User already exists"},{status : 400})
+    }
+
+    const hashpass = await bcrypt.hash(password,10);
+
+    const newUser = await User.create({
+      name,
+      email,
+      password : hashpass,
+    })
+
+    return NextResponse.json({message : "User succesfully created",newUser},{status : 200})
+
+  } catch (error) {
+    return NextResponse.json({ error: "Something went wrong during signup" },{status : 401});
+  }
+}
