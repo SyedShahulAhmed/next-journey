2bee808d9fc15f6f1cea8458f0e87089d2f06ef2
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2026-05-13 22:31:11 +0530

    add : User fetching route

---
+++ 18-url-shortener-api/src/app/api/auth/me/route.ts
@@ -0,0 +1,38 @@
+import { connectDb } from "@/lib/db";
+import { verifyToken } from "@/lib/jwt";
+import User from "@/models/User";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  try {
+    await connectDb();
+
+    const token = req.cookies.get("token")?.value;
+
+    if (!token) {
+      return NextResponse.json(
+        { error: "Unauthorized access" },
+        { status: 400 },
+      );
+    }
+
+    const decoded: any = verifyToken(token);
+
+    if (!decoded) {
+      return NextResponse.json({ error: "Invalid Token" }, { status: 400 });
+    }
+
+    const user = await User.findById(decoded.userId).select("-password");
+
+    if (!user) {
+      return NextResponse.json({ error: "Invalid User" });
+    }
+
+    return NextResponse.json({ user });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Something went wrong during user fetching" },
+      { status: 401 },
+    );
+  }
+}
