1431a1059aed8e85ea425ad9b37cf4855cf13ae2
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2026-05-14 22:28:26 +0530

    add : Auth middleware for dashboard

---
+++ 18-url-shortener-api/src/middleware/middleware.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const isAuthPage =
+    req.nextUrl.pathname.startsWith("/login") ||
+    req.nextUrl.pathname.startsWith("/signup");
+
+  const isProtected = req.nextUrl.pathname.startsWith("/dashboard");
+
+  const token = req.cookies.get("token")?.value;
+
+  if (isProtected && !token) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  if (isAuthPage && token) {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/dashboard/*", "/login", "signup"],
+};
