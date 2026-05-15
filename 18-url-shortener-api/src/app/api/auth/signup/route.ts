46e47ce7d16620ad88e58a5de9b9cfb5bc2f9853
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2026-05-15 22:03:55 +0530

    update : Formatted the signup route

--- 18-url-shortener-api/src/app/api/auth/signup/route.ts
+++ 18-url-shortener-api/src/app/api/auth/signup/route.ts
@@ -7,28 +7,39 @@ export async function POST(req: Request) {
   try {
     await connectDb();
 
-    const { name, email, password } = await req.json();    
+    const { name, email, password } = await req.json();
 
-    if(!name && ! email && !password) {
-      return NextResponse.json({error : "Some field are missing"},{status : 400})
-    }                            
-    const userExist = await User.findOne({email});
+    if (!name && !email && !password) {
+      return NextResponse.json(
+        { error: "Some field are missing" },
+        { status: 400 },
+      );
+    }
+    const userExist = await User.findOne({ email });
 
-    if(userExist) {
-      return NextResponse.json({error : "User already exists"},{status : 400})
+    if (userExist) {
+      return NextResponse.json(
+        { error: "User already exists" },
+        { status: 400 },
+      );
     }
 
-    const hashpass = await bcrypt.hash(password,10);
+    const hashpass = await bcrypt.hash(password, 10);
 
     const newUser = await User.create({
       name,
       email,
-      password : hashpass,
-    })
+      password: hashpass,
+    });
 
-    return NextResponse.json({message : "User succesfully created",newUser},{status : 200})
-
+    return NextResponse.json(
+      { message: "User succesfully created", newUser },
+      { status: 200 },
+    );
   } catch (error) {
-    return NextResponse.json({ error: "Something went wrong during signup" },{status : 401});
+    return NextResponse.json(
+      { error: "Something went wrong during signup" },
+      { status: 401 },
+    );
   }
 }
