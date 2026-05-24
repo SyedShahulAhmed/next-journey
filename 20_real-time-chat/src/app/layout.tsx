12bd5c7520ceedafac4bba06a131bffc7c4886d2
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2026-05-24 21:35:29 +0530

    Added default layout

---
+++ 20_real-time-chat/src/app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+
+import "../styles/globals.css";
+
+export const metadata: Metadata = {
+  title: "Real-Time Chat",
+  description: "A simple real-time chat application built with Next.js",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="h-full antialiased">
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
