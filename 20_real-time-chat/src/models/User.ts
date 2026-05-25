9bc54bb69175a25ac9d8a32990232a1fca5a0757
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2026-05-25 20:16:48 +0530

    Add : User Schema model

---
+++ 20_real-time-chat/src/models/User.ts
@@ -0,0 +1,30 @@
+import { model, models, Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+      default : "",
+    },
+  },
+  { timestamps: true },
+);
+
+const User = models.User || model("User", userSchema);
+
+export default User;
