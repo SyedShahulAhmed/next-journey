a5a9abf7d69666d89b1e8fabe74316e845f47cdb
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2026-05-30 22:22:20 +0530

    fix : minor Jwt fixes

--- 20_real-time-chat/src/lib/jwt.ts
+++ 20_real-time-chat/src/lib/jwt.ts
@@ -1,24 +1,21 @@
 import jwt from "jsonwebtoken";
 
-
 const JWT_SECRET = process.env.JWT_SECRET;
 
-if(!JWT_SECRET) {
-    throw new Error("Please define JWT SECRET")
+if (!JWT_SECRET) {
+  throw new Error("Please define JWT SECRET");
 }
-
 
 export function signToken(payload: { userId: string; email: string }) {
-    const token = jwt.sign(payload,JWT_SECRET,{
-        expiresIn : "7d",
-    })
-    return token;
+  return jwt.sign(payload, JWT_SECRET, {
+    expiresIn: "7d",
+  });
 }
 
-export function verifyToken(token : string){
-    try {
-        return jwt.verify(token,JWT_SECRET);
-    } catch (error) {
-        return null;
-    }
+export function verifyToken(token: string) {
+  try {
+    return jwt.verify(token, JWT_SECRET);
+  } catch {
+    return null;
+  }
 }
